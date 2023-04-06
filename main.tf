c7f1f115ba29a0e05064b2de550d9fcc76a7e43c
Derek <2476173+cailen@users.noreply.github.com>
2023-04-05 23:16:11 -0400

    test (#2)

--- main.tf
+++ main.tf
@@ -22,3 +22,10 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket_prefix = "my_bucket"
   acl           = "private"
 }
+
+resource "aws_s3_bucket_public_access_block" "my_bucket" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
